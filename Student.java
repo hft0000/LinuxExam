e618e702c893067fa63074b0382a019d905a938e
Your Name <you@example.com>
2021-12-31 23:24:06 +0800

    my first commit

---
+++ Student.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Student {
+    int id;
+    String name;
+    int age;
+  @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+}
